71c43903ad3525f8eceba6e59dd045322ae131fb
Oscar Merino <merinoaquino.oscar@gmail.com>
2017-06-16 00:35:32 -0600

    cuestionario casi completo, faltan detalles

--- src/main/java/com/bolsaTrabajo/controller/QuestionaryController.java
+++ src/main/java/com/bolsaTrabajo/controller/QuestionaryController.java
@@ -1,6 +1,7 @@
 package com.bolsaTrabajo.controller;
 
 
+import com.bolsaTrabajo.model.Exam;
 import com.bolsaTrabajo.model.User;
 import com.bolsaTrabajo.service.ExamService;
 import com.bolsaTrabajo.service.QuestionaryService;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,6 +31,7 @@ public class QuestionaryController {
         User u = userService.findByUsername(Auth.auth().getName());
         return u;
     }
+
     @RequestMapping(value = "/examenes/cuestionario", method = RequestMethod.GET)
     public String index(Model model){
         model.addAttribute("user", Auth.auth());
@@ -36,4 +39,12 @@ public class QuestionaryController {
         return "admin/exams/questionary/index";
     }
 
+    @RequestMapping(value = "/examenes/cuestionario/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable long id, Model model){
+        //el id que recibe es el de examen
+        Exam exam = examService.findById(id);
+        model.addAttribute("user", Auth.auth());
+        model.addAttribute("examen",exam);
+        return "admin/exams/questionary/cuestionario";
+    }
 }
---
+++ src/main/java/com/bolsaTrabajo/model/ExamResult.java
@@ -0,0 +1,53 @@
+package com.bolsaTrabajo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+
+public class ExamResult {
+    /*
+    private long id;
+    private Questionary questionary;
+    private Postulant postulant;
+    private String respuesta;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JsonIgnore
+    public Questionary getQuestionary() {
+        return questionary;
+    }
+
+    public void setQuestionary(Questionary questionary) {
+        this.questionary = questionary;
+    }
+
+    @ManyToOne
+    @JsonIgnore
+    public Postulant getPostulant() {
+        return postulant;
+    }
+
+    public void setPostulant(Postulant postulant) {
+        this.postulant = postulant;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+    */
+}
